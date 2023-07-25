80a0810043bb873cec2ac366e9ff0c70de02738f
Al Samad Shameem <124865334+alsamadxd@users.noreply.github.com>
2023-07-25 09:26:51 -0700

    Update q3.cpp
    Comments are added

--- Q-3/q3.cpp
+++ Q-3/q3.cpp
@@ -10,15 +10,19 @@ using namespace std;
 
 int main(){
     int r,c;
-    
+
+    // Get the no. of rows and columns of the matrices
     cout<<"Enter the no. of rows"<<endl;
     cin>>r;
     cout<<"Enter the no. of columns"<<endl;
     cin>>c;
 
+
+    // 2D Array declaration
     int a[r][c];
     int b[r][c];
 
+    // Getting the elements of 1st Matrix
     cout<<"Enter the elements of 1st matrix"<<endl;
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
@@ -27,6 +31,7 @@ int main(){
     }
 
 
+    // Getting the elements of 1st Matrix
     cout<<"Enter the elements of 2nd matrix"<<endl;
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
@@ -34,6 +39,7 @@ int main(){
         }
     }
 
+    // Display the Resultant Matrix
     cout<<"Enter the elements of 2nd matrix"<<endl;
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
